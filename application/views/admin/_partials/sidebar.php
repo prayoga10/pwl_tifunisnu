dec801a6c3526c7fb3a590c960c85f2497a86585
davidgeofano <davidjepara554@gmail.com>
2020-04-25 22:09:22 +0700

    first commit

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -3,28 +3,28 @@
     <li class="nav-item <?php echo $this->uri->segment(2) == '' ? 'active': '' ?>">
         <a class="nav-link" href="<?php echo site_url('admin') ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Overview</span>
+            <span>Beranda</span>
         </a>
     </li>
     <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'products' ? 'active': '' ?>">
         <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
             aria-expanded="false">
             <i class="fas fa-fw fa-boxes"></i>
-            <span>Products</span>
+            <span>Barang</span>
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="<?php echo site_url('admin/products/add') ?>">New Product</a>
-            <a class="dropdown-item" href="<?php echo site_url('admin/products') ?>">List Product</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/products/add') ?>">Barang Baru</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/products') ?>">Daftar Barang</a>
         </div>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="#">
             <i class="fas fa-fw fa-users"></i>
-            <span>Users</span></a>
+            <span>Pengguna</span></a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="#">
             <i class="fas fa-fw fa-cog"></i>
-            <span>Settings</span></a>
+            <span>Pengaturan</span></a>
     </li>
 </ul>
